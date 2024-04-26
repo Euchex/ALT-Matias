fd29ebb3d16e4429fb660ab832c49ceafd5c9db1
Euchex <111068143+Euchex@users.noreply.github.com>
2024-04-26 21:01:22 +0100

    With this code, when you click on a <p> tag, it will toggle the yellow-background class to change the background color, change the text color to black, and reduce the font weight to a lighter weight.

--- script.js
+++ script.js
@@ -5,8 +5,16 @@ const paragraphs = document.querySelectorAll('#skill p');
 paragraphs.forEach(paragraph => {
     paragraph.addEventListener('click', function() {
         // Remove yellow-background class from all paragraphs
-        paragraphs.forEach(p => p.classList.remove('yellow-background'));
+        paragraphs.forEach(p => {
+            p.classList.remove('yellow-background');
+            p.style.color = ''; // Reset text color to default
+            p.style.fontWeight = ''; // Reset font weight to default
+        });
         // Add yellow-background class to the clicked paragraph
         this.classList.add('yellow-background');
+        // Change text color to black
+        this.style.color = 'black';
+        // Reduce font-weight
+        this.style.fontWeight = 'lighter';
     });
 });
